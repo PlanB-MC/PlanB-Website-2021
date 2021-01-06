8350da20480c71b423f1df39554cb07e4d877103
MattCSmith <mrmcchow@gmail.com>
2021-01-06 04:12:08 +0000

    cleanup

--- src/templates/serverPage.js
+++ src/templates/serverPage.js
@@ -1,20 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
 import get from 'lodash/get'
-import Img from 'gatsby-image'
-import Layout from '../components/layout'
-
-import heroStyles from '../components/hero.module.css'
 import { MapEmbed, NavbarBacking, ServerPageContainer, ServerTitleContainer, ServerTitleName, ServerTitleSubtitle } from './serverPage.styles.js'
-import { Fragment } from 'react'
 import Navigation from '../components/navigation/navigation.component'
 import Footer from '../components/footer/footer.component'
 
 class ServerPageTemplate extends React.Component {
     render() {
         const server = get(this.props, 'data.contentfulServer')
-        const siteTitle = get(this.props, 'data.site.siteMetadata.title')
 
         return (
             <Fragment>
