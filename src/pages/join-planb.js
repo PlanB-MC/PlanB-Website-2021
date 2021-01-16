1aa4edd70a0930175a52383f57d5ed73482d3ce5
MattCSmith <mrmcchow@gmail.com>
2021-01-16 03:20:16 +0000

    Test

--- src/pages/join-planb.js
+++ src/pages/join-planb.js
@@ -63,6 +63,21 @@ const JoinPlanB = (props) => {
                         <button type="submit">Send</button>
                     </div>
                 </form>
+
+                <br />
+                <br />
+                <form name="Contact Form" method="POST" data-netlify="true">
+                    <input type="hidden" name="form-name" value="Contact Form" />
+                    <div>
+                        <label>Your Email:</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div>
+                        <label>Message:</label>
+                        <textarea name="message" />
+                    </div>
+                    <button type="submit">Send</button>
+                </form>
             </div>
 
         )
